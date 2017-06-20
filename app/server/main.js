78c4670f9cddb345b99e912676544a97c5fe942c
Callum Macdonald <git@callum-macdonald.com>
2017-06-20 18:31:32 +0200

    Update server main for examples.

--- app/server/main.js
+++ app/server/main.js
@@ -3,5 +3,12 @@ import { Meteor } from "meteor/meteor";
 import "/imports/server/getPromise.service";
 import "/imports/server/getPromise.methods";
 
-import "/imports/server/eg01";
-// import "/imports/server/eg02";
+import "/imports/server/getDataPromise.service";
+import "/imports/server/getDataPromise.methods";
+
+// import "/imports/server/meteor/meteor20";
+// import "/imports/server/meteor/meteor21";
+// import "/imports/server/meteor/meteor30";
+// import "/imports/server/meteor/meteor31";
+// import "/imports/server/meteor/meteor32";
+// import "/imports/server/meteor/meteor33";
