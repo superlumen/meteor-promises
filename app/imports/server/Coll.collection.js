332aa855bad65ecc6b6bf5a2c3c2d2edb8c9c251
Callum Macdonald <git@callum-macdonald.com>
2017-06-20 17:10:53 +0200

    Add some more docs to Coll.

--- app/imports/server/Coll.collection.js
+++ app/imports/server/Coll.collection.js
@@ -4,7 +4,10 @@ const Coll = new Mongo.Collection("coll");
 
 // Insert a doc if one doesn't exist
 if (Coll.find().count() === 0) {
-  Coll.insert({ a: "alpha", b: "bravo", c: { d: "delta" } });
+  Coll.insert({ x: "a", a: "alpha", b: "bravo", c: { d: "delta" } });
+  Coll.insert({ x: "b", a: "alpha", b: "bravo", c: { d: "delta" } });
+  Coll.insert({ x: "c", a: "alpha", b: "bravo", c: { d: "delta" } });
+  Coll.insert({ x: "d", a: "alpha", b: "bravo", c: { d: "delta" } });
 }
 
 export default Coll;
